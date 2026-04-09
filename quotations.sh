a81bf9923697eb19ec7ef6c00c40a9e3402775ec
123henry123 <henry@henryVM.myguest.virtualbox.org>
2026-04-09 10:49:52 -0500

    Added description for quotations

---
+++ quotations.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Quotes are usually used to start and end strings
+#There are two main types of quotes '' single quotes and "" double quotes
+#Single quotes display exactly what is between them meaining variables will not be called correctly.
+#Double quotes will expand anything that is between them.
