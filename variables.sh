8021fe3160615431357c7ac8b5c46dd91680dfa3
123henry123 <henry@henryVM.myguest.virtualbox.org>
2026-04-09 10:37:10 -0500

    added examples on how to set, use/call, and name variables

--- variables.sh
+++ variables.sh
@@ -5,3 +5,20 @@
 #to use quotes will help with that. A common issue is that syntax varies from
 #language to language. A way to troubleshoot is to search up to syntax for 
 #the language you are using.
+
+#Variables should have a name and not random letters and numbers put together
+#eg
+number_of_boxes="10"
+#bash is a loosely typed language meaning everything is turned into a string 
+#by default
+
+#This sets the value of number of boxes to 10
+#To call a variable in bash simple put the $ symbol before it
+#eg
+$number_of_boxes
+
+#However, that line would return a because your just saying the value and not 
+#saying what to do with it
+#A common way to use a variable is the use the "echo" command
+#eg
+echo "$number_of_boxes"
