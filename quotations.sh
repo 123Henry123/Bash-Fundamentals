e4f8a6d62512752289d763b8d39496913879e8fb
123henry123 <henry@henryVM.myguest.virtualbox.org>
2026-04-13 10:03:11 -0500

    added examples for quotations

--- quotations.sh
+++ quotations.sh
@@ -2,5 +2,16 @@
 
 #Quotes are usually used to start and end strings
 #There are two main types of quotes '' single quotes and "" double quotes
-#Single quotes display exactly what is between them meaining variables will not be called correctly.
+#Single quotes display exactly what is between them meaining variables will 
+#not be called correctly, but it will display the text literally.
 #Double quotes will expand anything that is between them.
+
+#eg)
+#lets say that
+number_of_boxes=10
+
+#if you were asked to show the number of boxes you would use double quotes
+echo "there are $number_of_boxes boxes"
+
+#if you were asked to show which variable stores the number of boxes you could use songle quotes
+echo '$number_of_boxes is the variable that stores the number of boxes'
--- variables.sh
+++ variables.sh
@@ -16,9 +16,9 @@ number_of_boxes="10"
 #To call a variable in bash simple put the $ symbol before it
 #eg
 $number_of_boxes
-
-#However, that line would return a because your just saying the value and not 
+#However, that line would return an error because your just saying the value and not 
 #saying what to do with it
+
 #A common way to use a variable is the use the "echo" command
 #eg
 echo "$number_of_boxes"
