aadd9e7d5b0fe7c1cd4d9b965453757db3d7897d
123henry123 <henry@henryVM.myguest.virtualbox.org>
2026-04-13 10:26:12 -0500

    listed all types of brackets that were covered

---
+++ brackets.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#there are many different type of brackets in bash
+#every bracket performs a different action
+#the different type of brackets include:
+#() single round brackets
+#(()) double round brackets
+#[] square brackets
+#[[]] double square brackets
+#{} curly brackets
