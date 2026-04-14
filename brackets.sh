2e5e99b305baec77a42bf8db8782f176827d7f7c
123henry123 <henry@henryVM.myguest.virtualbox.org>
2026-04-14 10:24:08 -0500

    added examples for single and double square brackets

--- brackets.sh
+++ brackets.sh
@@ -20,6 +20,19 @@ list_of_fruits=(apple orange bananas)
 echo $((1+1))
 
 #single square brackets are used to test if things are true or false
+#eg
+number_of_boxes="1"
+[ $number_of_boxes ]
+#the brackets will check if there is any value stored within number_of_boxes
+#regardless of what the value is
 
 #double square brackets are used to compare things and you are able 
 #to use: -lt, -eq, -gt, &&, and many other types of comparison
+#eg
+number_of_computers="32"
+if [[ $number_of_boxes -lt $number_of_computers ]]; then
+	echo "there are more computers than boxes" 
+fi
+#this will compare the variables number_of_boxes and number_of_computers
+#if the number of boxes is less than the number of computers it will show
+#the line "there are more computers than boxes"
