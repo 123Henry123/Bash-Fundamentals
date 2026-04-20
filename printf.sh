c928bc472728f99c9056f2b224464dc14e695aaf
123henry123 <henry@henryVM.myguest.virtualbox.org>
2026-04-20 10:47:04 -0500

    added descriptiton for printf

---
+++ printf.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#printf is similar to echo where they both display text onto your screen.
+#however, printf has a lot more options than echo. meaning it is able to
+#display text in ways that echo is unable to.
