cbea3a8cb02abecb415d66a48460e643e0bc1d3a
123henry123 <henry@henryVM.myguest.virtualbox.org>
2026-04-21 10:08:39 -0500

    added example for printf

--- printf.sh
+++ printf.sh
@@ -3,3 +3,10 @@
 #printf is similar to echo where they both display text onto your screen.
 #however, printf has a lot more options than echo. meaning it is able to
 #display text in ways that echo is unable to.
+
+#eg
+printf "\nhello\n\n"
+
+#if we were to use echo we would need to add a -e tag 
+#however with printf you don't need any tags to perform options such as \n
+
--- variables.sh
+++ variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Variables are basically boxes that hold anything that can fit inside it
-#Common issues forgetting quatations is a common mistake simply remembering 
+#Common issues forgetting quotations is a common mistake simply remembering 
 #to use quotes will help with that. A common issue is that syntax varies from
 #language to language. A way to troubleshoot is to search up to syntax for 
 #the language you are using.
