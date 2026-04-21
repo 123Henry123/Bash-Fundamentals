707522939805f7d082144e575ab24419a09b041d
123henry123 <henry@henryVM.myguest.virtualbox.org>
2026-04-21 10:45:58 -0500

    started adding descriptions for if else and elif statements (end of class commit)

---
+++ if_else_elif.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# "if" statements are used to set conditions for a script or function to run
+# "elif" statements are used if you want to set another condition
+# "else" statements are used to run a script designated for all results that
+# do not meet any previously stated conditions
+# 
