f8af778d13dde5238e24d843bd914d017fc93454
123henry123 <henry@henryVM.myguest.virtualbox.org>
2026-04-22 10:05:44 -0500

    finished adding descriptions for if else and elif statements

--- if_else_elif.sh
+++ if_else_elif.sh
@@ -4,4 +4,5 @@
 # "elif" statements are used if you want to set another condition
 # "else" statements are used to run a script designated for all results that
 # do not meet any previously stated conditions
-# 
+#The commands under if and elif will only run if certain conditions are met
+
