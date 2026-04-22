da80f7bf480272a746b69cbee4d52a9f3c6d25b9
123henry123 <henry@henryVM.myguest.virtualbox.org>
2026-04-22 10:14:55 -0500

    added example for if else and elif

--- if_else_elif.sh
+++ if_else_elif.sh
@@ -6,3 +6,11 @@
 # do not meet any previously stated conditions
 #The commands under if and elif will only run if certain conditions are met
 
+#eg
+number_of_boxes="3"
+if [[ $number_of_boxes="3" ]]; then
+	echo "there are 3 boxes"
+elif [[ $number_of_boxes="2" ]]; then
+	echo "there are 2 boxes"
+else
+	echo "you either have more than 3 boxes or less than 2 boxes"
