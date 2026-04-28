9023031c56cf2548008947b5a931132fecb77d13
123henry123 <henry@henryVM.myguest.virtualbox.org>
2026-04-28 10:30:51 -0500

    added descriptions for case statements

---
+++ case.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#case statements are used instead of repetitively using if else and elif statements.
+#The case statement checks if the value of the variable is equal to a value or multiple
+#that you have set.
