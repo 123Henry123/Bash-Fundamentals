1833bb9b849ddf5352d33c067ddc154b685ec4bc
123henry123 <henry@henryVM.myguest.virtualbox.org>
2026-04-28 10:47:12 -0500

    started making example for case statements (end of class commit)

--- case.sh
+++ case.sh
@@ -3,3 +3,14 @@
 #case statements are used instead of repetitively using if else and elif statements.
 #The case statement checks if the value of the variable is equal to a value or multiple
 #that you have set.
+
+#eg
+
+number_of_boxes="5"
+
+case word in
+	pattern)
+		;;
+	*)
+		;;
+esac
