2e096e10e4fd46e18051fe0b64c3a35993446738
123henry123 <henry@henryVM.myguest.virtualbox.org>
2026-04-29 15:53:37 -0500

    added example and short explaination for arrays

--- arrays.sh
+++ arrays.sh
@@ -2,3 +2,9 @@
 
 #arrays are variables that are able to store multiple values
 #an anology is a big box where you can put a bunch of things within it.
+
+#eg
+array=("tree" "hi")
+echo "${array[1]}"
+
+#this will print "hi" because the first indes is always 0
