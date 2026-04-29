3ace0b3597cc881d396503ab8a45ba732e8d4953
123henry123 <henry@henryVM.myguest.virtualbox.org>
2026-04-29 09:49:58 -0500

    added example for case statement

--- case.sh
+++ case.sh
@@ -8,9 +8,16 @@
 
 number_of_boxes="5"
 
-case word in
-	pattern)
+case $number_of_boxes in
+	1)
+		echo "there are 5 boxes"
+		;;
+	2)
+		echo "there are 2 boxes"
+		;;
+	3)
+		echo "there are 3 boxes"
 		;;
 	*)
-		;;
+		echo "there are more than 3 boxes"
 esac
