0dca23490a0de8693d8c3fdcc88e3f5a122122da
123henry123 <henry@henryVM.myguest.virtualbox.org>
2026-04-29 10:31:15 -0500

    added example for read command

--- user_input.sh
+++ user_input.sh
@@ -3,3 +3,9 @@
 #user input is a variable the the user running the script inputs while the script is running
 #to obtain a user input from a user you must promt them to enter a value
 #to do that you can use the read command.
+
+#eg
+read user_input
+
+#that will promt the user to type something. however, it will look very borring
+#and desn't have any question attached to it.
