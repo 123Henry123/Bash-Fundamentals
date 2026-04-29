ff9fb9e4f75a2acd09dd07a17cb15430b24a7060
123henry123 <henry@henryVM.myguest.virtualbox.org>
2026-04-29 16:08:36 -0500

    added common issues with arrays and how to troubleshoot

--- arrays.sh
+++ arrays.sh
@@ -8,3 +8,8 @@ array=("tree" "hi")
 echo "${array[1]}"
 
 #this will print "hi" because the first indes is always 0
+
+#a common mistake when callaing a variable stored in an array is thinking that the
+#first value in the array is indexed as 1. To figure out whether it is base 0
+#(first value is 0) or base 1 (first value is one). you can check manuals such as
+#the bash manual.
