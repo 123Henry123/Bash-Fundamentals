39d1118a494814c7efe7b94831c58cdf24bdfc67
123henry123 <henry@henryVM.myguest.virtualbox.org>
2026-04-29 10:29:39 -0500

    added description for user_inputs

---
+++ user_input.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#user input is a variable the the user running the script inputs while the script is running
+#to obtain a user input from a user you must promt them to enter a value
+#to do that you can use the read command.
