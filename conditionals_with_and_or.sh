7980a8087ad7da2939451d8f2a465f4423d82d14
123henry123 <henry@henryVM.myguest.virtualbox.org>
2026-04-30 10:24:26 -0500

    added how to use and/or and formated descriptions for and/or

--- conditionals_with_and_or.sh
+++ conditionals_with_and_or.sh
@@ -3,10 +3,11 @@
 #if you want something to run if multiple conditions are met or if 1 of the multiple
 #conditions are met you can include and/or into your conditions.
 
-#and (&&):
-#the and operator is used when you want multiple contitions to be met before running
-#the following program. 
+#---------------------------------------|-------------------------------------------|
+#and (&&):								|or (||):									|
+#the and operator is used when you want	|the or operator is used when you want one 	|
+#multiple contitions to be met before 	|of the following conditions to be met 		|
+#running the following program. 		|before running the following program.		|
+#---------------------------------------|-------------------------------------------|
 
-#or (||):
-#the or operator is used when you want one of the following conditions to be met
-#before running the following program.
+#to use and/or operators you just need to insert them between your conditions
