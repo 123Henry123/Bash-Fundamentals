896ce0fcea8938605238a65dd0f08b85a73e31d3
123henry123 <183658424+123Henry123@users.noreply.github.com>
2026-05-06 10:10:08 -0500

    added description for functions

---
+++ functions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Bash has many built in functions such a read, echo, case, and many more
+#However, there is not a function for everything. 
+
+#If there is something that you want to be done repeatedly you can make 
+#your own function.
+
+#You can think of a function a a tool there isn't a tool for everything
+#but if you make a tool you can use it infinitely until it breaks
