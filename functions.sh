c6b7ae3541b6ca5eee0ff762398a5b6fabc23d5f
123henry123 <183658424+123Henry123@users.noreply.github.com>
2026-05-06 10:18:31 -0500

    added examples for making and running a function

--- functions.sh
+++ functions.sh
@@ -7,4 +7,15 @@
 #your own function.
 
 #You can think of a function a a tool there isn't a tool for everything
-#but if you make a tool you can use it infinitely until it breaks
+#but if you make a tool you can use it infinitely until it breaks.
+
+#eg
+
+example_function() {
+	echo "this is a function:"
+	printf "some things to remember\ncreating the function does not run the function\nto run the function you must use the name of the function\n"
+	
+}
+
+example_function
+
