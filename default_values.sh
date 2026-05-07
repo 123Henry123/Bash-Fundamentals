49151596c35305446fdd2470e14b12480b10b6e4
123henry123 <183658424+123Henry123@users.noreply.github.com>
2026-05-07 10:24:41 -0500

    added description for default values

---
+++ default_values.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#default values are used primarily in three ways:
+#	1. as a fallback method
+#	2. default value when variable is empty
+#	3. default value when variable is not set	
+
+#default values exist to make code more stable
+#it acts like a safety net
