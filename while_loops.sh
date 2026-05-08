09b3320eaccdb72281ff37858c392978f1e2079c
123henry123 <183658424+123Henry123@users.noreply.github.com>
2026-05-08 10:23:05 -0500

    added description for while loops

---
+++ while_loops.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#while loops are a type of loop that will repeat the function(s) inside of them
+#while a certain condition is met.
+
+#there are two ways to end a while loop
+#1. the condition to run the loop is no longer met
+#2. putting 'break' in the will end the loop
