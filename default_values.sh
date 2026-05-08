73d194446f7197b1f382d97f10638c61aa172791
123henry123 <183658424+123Henry123@users.noreply.github.com>
2026-05-08 10:09:27 -0500

    added common mistakes and ways to troubleshoot for default variables

--- default_values.sh
+++ default_values.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #default values are used primarily in three ways:
-#	1. as a fallback method
+#	1. as a fallback method (when variable is unset or empty)
 #	2. default value when variable is empty
 #	3. default value when variable is not set	
 
@@ -11,10 +11,18 @@
 #eg
 
 #fallback method
-----------------------------------------
-example="hello"
-example_for_fallback=${example:-default}
-echo "$example_for_fallback"
+----------------------------------------------------------------------------|
+example="hello"																|
+example_for_fallback=${example:-default}									|
+echo "$example_for_fallback"												|
+#																			|				
+#this will return 'hello' even though the default value was set after		|
+#the 'fallback method' will only print if the variable is empty or unset	|
+----------------------------------------------------------------------------|
 
-#this will return 'hello' even though the default value was set after
-#the 'fallback method' will only print if the variable is empty or unset
+
+#common mistakes when setting default values
+#a common mistake is thinking that the new variable needs to be the same as the 
+#one you are trying to set a default value for.
+#In reality the variable that you are trying to set the default values for is
+#the variable that goes into the curly brackets
