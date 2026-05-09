5768589691ca127cd4fc191671154a9a15e0a787
123Henry123 <183658424+123Henry123@users.noreply.github.com>
2026-05-09 20:18:53 +0000

    started example for 2 dimensional arrays

--- two_dimensional_arrays.sh
+++ two_dimensional_arrays.sh
@@ -2,4 +2,17 @@
 
 #the two dimentional array is a way to store store values like in a table
 #as the name suggests it is just an array with an extra dimension added to it (vertically)
-#unfortunately it is not supported on bash
+#unfortunately two dimensional arrays are not supported on bash
+
+#eg)
+#a two dimensional array would look like this (in python):
+
+#example = [
+#    [1, 2, 3],
+#    [4, 5, 6],
+#    [7, 8, 9]
+#]
+#print(example[0][1])
+
+#however bash does not support two dimensional arrays but has a different
+#of doing it
