4ca18efc97f46b53a579e8c408ad5a787db8295a
123Henry123 <183658424+123Henry123@users.noreply.github.com>
2026-05-09 20:25:40 +0000

    added example of associative arrays

--- two_dimensional_arrays.sh
+++ two_dimensional_arrays.sh
@@ -15,4 +15,21 @@
 #print(example[0][1])
 
 #however bash does not support two dimensional arrays but has a different
-#of doing it
+#of doing it called an associative array
+
+#eg)
+declare -A example
+example[0,0]=1
+example[0,1]=2
+example[0,2]=3
+
+example[1,0]=4
+example[1,1]=5
+example[1,2]=6
+
+example[2,0]=7
+example[2,1]=8
+example[2,2]=9
+
+echo "${example[0,1]}"
+
