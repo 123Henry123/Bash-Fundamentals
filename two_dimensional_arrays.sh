459c038de33fe4f6def14de27fd974ce714d30b6
123Henry123 <183658424+123Henry123@users.noreply.github.com>
2026-05-09 20:06:07 +0000

    added description fo two dimensional arrays

---
+++ two_dimensional_arrays.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#the two dimentional array is a way to store store values like in a table
+#as the name suggests it is just an array with an extra dimension added to it (vertically)
+#unfortunately it is not supported on bash
