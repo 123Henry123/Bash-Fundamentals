625e18816510d317add519be32cd3d9513d2e6ac
123henry123 <183658424+123Henry123@users.noreply.github.com>
2026-05-12 10:17:50 -0500

    added different types of redirects

--- redirects.sh
+++ redirects.sh
@@ -2,4 +2,7 @@
 
 #redirects are used to edit a file without actually opening it
 #they come in a variety of symbols depending on how you want to edit
-#the file
+#the file.
+#there are standard inputs (stdin)
+#there are standard outputs (stdout)
+#there are standard error (stderr)
